d833d526e92e2e87b28bd536525ee21940ebe8a4
Vinicius <vtmsugimoto@gmail.com>
2019-04-18 15:43:04 -0300

    Definição de structs e início da leitura do bytecode.

--- main.c
+++ main.c
@@ -7,8 +7,47 @@ typedef uint16_t u2;
 typedef uint32_t u4;
 
 #define u1_read(buffer, file) { fread(&buffer, sizeof(u1), 1, file); }
+//#define u2_read(buffer, file) { fread(&buffer, sizeof(u2), 1, file); }
+#define u4_read(buffer, file) { fread(&buffer, sizeof(u4), 1, file); }
+#define magic_read(buffer, file) { int i; for(i = 0; i < 4; i++){u1 byte; u1_read(byte, file); buffer <<= i == 0 ? 0 : 8; buffer |= byte;} }
 #define u2_read(buffer, file) { int i; for(i = 0; i < 2; i++){u1 byte; u1_read(byte, file); buffer <<= i == 0 ? 0 : 8; buffer |= byte;} }
-#define u4_read(buffer, file) { int i; for(i = 0; i < 4; i++){u1 byte; u1_read(byte, file); buffer <<= i == 0 ? 0 : 8; buffer |= byte;} }
+
+typedef struct {
+  u1 tag;
+  u1 *info;
+} cp_info;
+
+typedef struct {
+
+} field_info;
+
+typedef struct {
+
+} method_info;
+
+typedef struct {
+
+} attribute_info;
+
+typedef struct {
+  u4 magic;
+  u2 minor_version;
+  u2 major_version;
+  u2 const_pool_count;
+  cp_info *constant_pool;
+  u2 access_flags;
+  u2 this_class;
+  u2 super_class;
+  u2 interfaces_count;
+  u2 *interfaces;
+  u2 fields_count;
+  field_info *fields;
+  u2 methods_count;
+  method_info *methods;
+  u2 attributes_count;
+  attribute_info *attributes;
+} ClassFile;
+
 
 int main(int argc, char **argv){
   if(argc < 2){
@@ -16,23 +55,29 @@ int main(int argc, char **argv){
     exit(-1);
   }
   char *filename = argv[1];
-  FILE *class = fopen(filename, "rb");
+  FILE *classfile = fopen(filename, "rb");
 
-  u4 cafe;
-  u4_read(cafe, class);
+  ClassFile *class = (ClassFile *) malloc(sizeof(ClassFile));
 
-  printf("%x\n", cafe);
+  magic_read(class->magic, classfile);
+  u2_read(class->minor_version, classfile);
+  u2_read(class->major_version, classfile);
+  u2_read(class->const_pool_count, classfile);
+  class->constant_pool = (cp_info *) malloc(sizeof(cp_info) * class->const_pool_count);
+  for(int i = 0; i < class->const_pool_count; i++){
 
-  u2 m, M;
-  u2_read(m, class);
-  u2_read(M, class);
+  }
 
-  printf("Minor: %x\nMajor: %x\n", m, M);
-
-  u2 const_pool_count;
-  u2_read(const_pool_count, class);
-
-  printf("CPC: %x (%hu)\n", const_pool_count, const_pool_count);
+  printf(
+    "Cafe: 0x%x\n"
+    "Minor: 0x%x\n"
+    "Major: 0x%x\n"
+    "CPC: 0x%x\n",
+    class->magic,
+    class->minor_version,
+    class->major_version,
+    class->const_pool_count
+  );
 
   return 0;
 }
