b11a9ac9d6f43a4f172c5eb5d6a4158ef394bbc5
Vinicius <vtmsugimoto@gmail.com>
2019-05-22 14:53:53 -0300

    Resolvido o problema do 0xdafebabe no windows

--- src/bread_classfile.c
+++ src/bread_classfile.c
@@ -4,47 +4,47 @@ extern int verbose;
 
 ClassFile * bread_classfile(FILE *classfile){
 
-  ClassFile *class = (ClassFile *) malloc(sizeof(ClassFile));
-  if(verbose) printf("Alloc'd memory for ClassFile\n");
+	ClassFile *class = (ClassFile *) calloc(1, sizeof(ClassFile));
+	if(verbose) printf("Alloc'd memory for ClassFile\n");
 
-    /* Magic number */
-  if(bread_magic(class, classfile)){
-    free(class);
-    fprintf(stderr, "Invalid .class file. Magic number \"0xcafebabe\" does not check.\n");
-    exit(ERR_MAGIC);
-  }
-  if(verbose) printf("Read magic number\n");
+		/* Magic number */
+	if(bread_magic(class, classfile)){
+		free(class);
+		fprintf(stderr, "Invalid .class file. Magic number \"0xcafebabe\" does not check.\n");
+		exit(ERR_MAGIC);
+	}
+	if(verbose) printf("Read magic number\n");
 
-    /* Minor and major versions */
-  bread_minor(class, classfile);
-  if(verbose) printf("Read minor version\n");
-  bread_major(class, classfile);
-  if(verbose) printf("Read major version\n");
-    /* Constant pool count and constant pool */
-  bread_constant_pool(class, classfile);
-  if(verbose) printf("Read constant pool\n");
-    /* Access flags, this class and super class */
-  bread_access_flags(class, classfile);
-  if(verbose) printf("Read access flags\n");
-  bread_this_class(class, classfile);
-  if(verbose) printf("Read this class\n");
-  bread_super_class(class, classfile);
-  if(verbose) printf("Read super class\n");
-    /* Interfaces count and interfaces */
-  bread_interfaces(class, classfile);
-  if(verbose) printf("Read interfaces\n");
-    /* Fields count and fields */
-  bread_fields(class, classfile);
-  if(verbose) printf("Read fields\n");
-    /* Methods */
-  bread_methods(class, classfile);
-  if(verbose) printf("Read methods\n");
-    /* Attributes */
-  bread_attributes_count(class, classfile);
-  if(verbose) printf("Read attributes count\n");
-  bread_attributes(class->attributes, class->attributes_count, classfile);
-  if(verbose) printf("Read attributes\n");
+		/* Minor and major versions */
+	bread_minor(class, classfile);
+	if(verbose) printf("Read minor version\n");
+	bread_major(class, classfile);
+	if(verbose) printf("Read major version\n");
+		/* Constant pool count and constant pool */
+	bread_constant_pool(class, classfile);
+	if(verbose) printf("Read constant pool\n");
+		/* Access flags, this class and super class */
+	bread_access_flags(class, classfile);
+	if(verbose) printf("Read access flags\n");
+	bread_this_class(class, classfile);
+	if(verbose) printf("Read this class\n");
+	bread_super_class(class, classfile);
+	if(verbose) printf("Read super class\n");
+		/* Interfaces count and interfaces */
+	bread_interfaces(class, classfile);
+	if(verbose) printf("Read interfaces\n");
+		/* Fields count and fields */
+	bread_fields(class, classfile);
+	if(verbose) printf("Read fields\n");
+		/* Methods */
+	bread_methods(class, classfile);
+	if(verbose) printf("Read methods\n");
+		/* Attributes */
+	bread_attributes_count(class, classfile);
+	if(verbose) printf("Read attributes count\n");
+	bread_attributes(class->attributes, class->attributes_count, classfile);
+	if(verbose) printf("Read attributes\n");
 
-  if(verbose) printf("Finished reading class\n");
-  return class;
+	if(verbose) printf("Finished reading class\n");
+	return class;
 }
--- src/main.c
+++ src/main.c
@@ -11,33 +11,33 @@
 int verbose = 1;
 
 int main(int argc, char **argv){
-  if(argc < 2){
-    fprintf(stderr, "Usage:\n\t%s <.class file name>\n", argv[0]);
-    exit(ERR_NOFILE);
-  }
+	if(argc < 2){
+		fprintf(stderr, "Usage:\n\t%s <.class file name>\n", argv[0]);
+		exit(ERR_NOFILE);
+	}
 
 	setlocale(LC_ALL, "");
-  char *filename = argv[1];
-  FILE *classfile = fopen(filename, "rb");
+	char *filename = argv[1];
+	FILE *classfile = fopen(filename, "rb");
 
-  if(classfile == NULL){
-    fprintf(stderr, "Failed to open specified file\n");
-    exit(ERR_CANTOPENFILE);
-  }
+	if(classfile == NULL){
+		fprintf(stderr, "Failed to open specified file\n");
+		exit(ERR_CANTOPENFILE);
+	}
 
-  if(verbose) printf("File opened\n");
-  
-  ClassFile *class = bread_classfile(classfile);
+	if(verbose) printf("File opened\n");
+	
+	ClassFile *class = bread_classfile(classfile);
 
-  if(verbose) printf("File read\n");
+	if(verbose) printf("File read\n");
 
-  bprint_classfile(class);
+	bprint_classfile(class);
 
-  bfree_classfile(class);
+	bfree_classfile(class);
 
-  fclose(classfile);
+	fclose(classfile);
 
-  return 0;
+	return 0;
 }
 
 
